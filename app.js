4015fabe9e0945ac4aaebcbe75a733aee13c1e38
Gasty Lobo <gastonlobo777@gmail.com>
2022-04-06 21:43:00 -0300

    Estructura de archivos

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
+const path = require("path");
+
+app.listen(3000, () => console.log("Puerto 3000 funcionando correctamente"));
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/index.html"))
+);
+
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/login.html"))
+);
+
+app.get("/productCart", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/productCart.html"))
+);
+
+app.get("/productDetail", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/productDetail.html"))
+);
+
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "./views/register.html"))
+);
---
+++ retro.md
@@ -0,0 +1,21 @@
+¿Qué hay que...
+
+1. Comenzar a hacer?
+
+
+
+2. Hacer más?
+
+
+
+3. Continuar haciendo?
+
+
+
+4. Hacer menos?
+
+
+
+5. Dejar de hacer?
+
+
