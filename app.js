7c40a9771bec2c2b67e8f67de5d47509ae6c475f
Gasty Lobo <gastonlobo777@gmail.com>
2022-05-04 13:46:28 -0300

    Cree EJS. Sistema de ruteo. Partials: Head, Header y Footer. Partials CSS para controlar los header desde un mismo css y  no tener que cambiar uno por uno. Cree el headerBlack.css y headerWhite.css porque hay paginas que el fondo es blanco entonces necesitamos el header de color gris/negro y viceversa. Lo mismo hice con los footers para poder manejar los estilos desde un mismo css para todas las paginas. Agregué que cuando se haga click al logo lleve al home

--- app.js
+++ app.js
@@ -5,24 +5,10 @@ const path = require("path");
 
 app.listen(3000, () => console.log("Puerto 3000 funcionando correctamente"));
 
+app.set('view engine', 'ejs')
+
 app.use("/static", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/index.html"))
-);
+let indexRoute = require('./routes/indexRoute')
 
-app.get("/login", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/login.html"))
-);
-
-app.get("/productCart", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/productCart.html"))
-);
-
-app.get("/productDetail", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/productDetail.html"))
-);
-
-app.get("/register", (req, res) =>
-  res.sendFile(path.join(__dirname, "./views/register.html"))
-);
+app.use('/', indexRoute);
---
+++ routes/indexRoute.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const router = express.Router();
+
+const controller = require('../controllers/indexController')
+
+router.get('/', controller.index);
+
+router.get('/login', controller.login);
+
+router.get('/productCart', controller.productCart);
+
+router.get('/productDetail', controller.productDetail);
+
+router.get('/register', controller.register);
+
+
+module.exports = router;    
