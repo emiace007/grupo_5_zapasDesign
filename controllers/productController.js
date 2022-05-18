26cca2f19df5c661e7a4f1d05a4ca90d4a1a03b0
AlanRcq <acueto@fi.uba.ar>
2022-05-18 13:40:32 -0300

    Agrego la funcionalidad de cargar un nuevo producto desde editProductos

--- controllers/productController.js
+++ controllers/productController.js
@@ -13,6 +13,39 @@ const controller = {
   }, 
   products: (req, res) => res.render("products", {productos:productos}),
   editProductos: (req, res) => res.render("editProductos", {productos:productos}),
+
+  createProduct: (req, res) => {
+    let newProduct = {
+      id: generarId(),
+      price: req.body.precio,
+      nombreProducto: req.body.producto,
+      image: req.file.filename,
+      category: ["deportes"],
+      description: req.body.descripcion,
+    }
+
+    productos.push(newProduct);
+    fs.writeFileSync(productsFilePath, JSON.stringify(productos))
+
+    res.redirect('/products')
+  }
+
+
 };
+
+//Hice esta función más que nada por miedo a que se puedan pisar las ids
+function generarId() {
+  let id = productos.length + 1;
+  while (true) {
+    if (productos.find(product => product.id == id)){
+      id++
+    }
+    else {
+      break
+    }
+  }
+  return id;
+}
+
 
 module.exports = controller;
--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,9 +1,28 @@
 const express = require ("express");
 const router = express.Router();
 
+const path = require('path')
+
 // >>>> Requiere
 
 const productController = require('../controllers/productController')
+
+// >>>> Multer
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, __dirname + '/../public/images/productos')
+    },
+
+    filename: function(req, file, callback){
+        const newFileName = Date.now() + path.extname(file.originalname);
+        callback(null, newFileName);
+    }
+})
+
+const fileUpload = multer({storage: storage})
 
 // >>>>> Rutas 
 
@@ -14,6 +33,8 @@ router.get('/productCart', productController.productCart); //products/cart
 router.get('/editProductos', productController.editProductos); //products/edit/:id?
 
 router.get('/:idProduct', productController.productDetail); //products/detail/:id
+
+router.post('/create', fileUpload.single('imagen'), productController.createProduct)
 
 
 module.exports = router;   
