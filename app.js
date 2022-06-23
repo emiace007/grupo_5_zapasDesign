6f39c7852116dac84b1d8ce4c99de337fa9d0616
camilopareja <camilopareja11@gmail.com>
2022-06-22 22:57:38 -0500

    organice estructura de app.js

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
 const express = require("express");
-const createError = require('http-errors');
 const app = express();
+const createError = require('http-errors');
 const methodOverride = require ("method-override")
 const path = require("path");
 const session = require('express-session')
 const cookies = require('cookie-parser')
 const recordarUsuarioMiddleware = require('./middlewares/recordarUsuarioMiddleware')
 
-// const cookieUser = require('./middlewares/cookieUser')
-// app.use(cookieUser)
-
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+//Inicio servidor para pruevas de desarrollo
 
 app.listen(3000, () => console.log("Puerto 3000 funcionando correctamente"));
 
 app.set('view engine', 'ejs')
 
-app.use(methodOverride ("_method"))
+//MIDDLEWARES
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 app.use("/static", express.static(__dirname + "/public"));
-app.use(session({
-    secret: 'Esto es un secreto',
+app.use(
+  session({
+    secret: "Esto es un secreto",
     resave: false,
-    saveUninitialized: false
-}))
-
-app.use(cookies())
-
-//Middleware para que todas las vistas puedan tener acceso al usuario
-app.use(function(req, res, next) {
-    res.locals.user = req.session.usuarioLogiado;
-    next();
-  });
-
-app.use(recordarUsuarioMiddleware)
+    saveUninitialized: false,
+  })
+);
+app.use(cookies());
+app.use(function (req, res, next) {
+  //Middleware para que todas las vistas puedan tener acceso al usuario
+  res.locals.user = req.session.usuarioLogiado;
+  next();
+});
+app.use(recordarUsuarioMiddleware); //Middleware para mantener una sesion inciada si el usuario asi lo requiere en el log in
 
 // RUTAS
 
-let indexRoute = require('./routes/indexRoute')
-app.use('/', indexRoute);
+let indexRoute = require("./routes/indexRoute");
+app.use("/", indexRoute);
 
-let productsRoute = require('./routes/productsRoute')
-app.use('/products', productsRoute);
+let productsRoute = require("./routes/productsRoute");
+app.use("/products", productsRoute);
 
-let userRoute = require('./routes/userRoute')
-app.use('/user', userRoute);
-
+let userRoute = require("./routes/userRoute");
+app.use("/user", userRoute);
