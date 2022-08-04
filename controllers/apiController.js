aa5649cc4b553279b2fc1f51f1476729b7b46f49
Alan_Ramiro_Cueto_Quinto <acueto@fi.uba.ar>
2022-08-03 23:31:37 -0300

    Agregada API de users

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,14 +4,28 @@ const controller = {
   users: (req, res) => 
   db.Users.findAll()
     .then((usuarios) => {
-     
+      
+      let usuariosNew = [];
+
+      usuarios.forEach( function( usuario ) {
+        usuariosNew.push({
+          id: usuario.id,
+          name: usuario.nombre,
+          email: usuario.email,
+          detail: "users/detail/" + usuario.id,
+        })
+      } )
+
+      console.log(usuarios[0].url)
+
       let objetoLiteral = {
         "count":usuarios.length,
-        "users": usuarios,  
+        "users": usuariosNew,  
         //detail
      }
       res.send(objetoLiteral)
     }),
+
   products: (req, res) => 
   db.Product.findAll()
     .then((productos) => {
