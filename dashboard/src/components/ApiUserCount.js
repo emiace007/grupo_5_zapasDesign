46a58db599e523de3c5fd6d9d0c828ffe8e84cad
Gasty Lobo <gastonlobo777@gmail.com>
2022-08-08 21:48:04 -0300

    Funciona el fetch

--- dashboard/src/components/ApiUserCount.js
+++ dashboard/src/components/ApiUserCount.js
@@ -1,32 +1,38 @@
 import { useEffect, useState } from "react";
 
 function ApiUserCount() {
-  const [informacion, setInformacion] = useState();
+  const [informacion, setInformacion] = useState([]);
 
-  const url = "http://localhost:3001/api/users";
-  // const url = "https://jsonplaceholder.typicode.com/todos";
+  const url = "/api/users";
 
   const fetchApis = async () =>{
     const response = await fetch(url)
-    const apiJSON = await response.json() 
-    setInformacion(apiJSON)
-    console.log(apiJSON);
+    const apiJSON = await response.json()
+    return setInformacion(apiJSON)
   }
 
   useEffect(() => {
     fetchApis()
   }, []);
 
+  useEffect(() => {
+    console.log('Actualizado');
+  }, [informacion]);
+ 
+  useEffect(() => {
+    return () => console.log('Componente desmontado');
+  }, []);
+
+
   return (
     <div>
       <h1>API USER COUNT</h1>
-      
-      {!informacion ? "Cargando..." : 
-      informacion.map((informacion) => {
-        return(
-          <h3>{informacion.count}</h3>
-        )
-      })}
+      <h3>{informacion.count}</h3>
+      {/* {!informacion
+        ? "Cargando..."
+        : informacion.map((informacion) => {
+            return <h3>{informacion.count}</h3>;
+          })} */}
     </div>
   );
 }
