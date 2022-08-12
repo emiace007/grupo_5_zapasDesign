3c8be79652584cc1d8dc51845ec9b034a82c0bc9
Gasty Lobo <gastonlobo777@gmail.com>
2022-08-11 23:25:01 -0300

    Sprint 7, realizamos los checkbox

--- public/js/create.js
+++ public/js/create.js
@@ -2,9 +2,14 @@ window.onload = function() {
         let nombre = document.getElementById('nombreProducto')
         let description = document.getElementById('description')
         let imagen = document.getElementById('files')
+        let price = document.getElementById('price')
+        let marca = document.getElementById('marca')
+        let categoria = document.querySelectorAll('.categoriaInput')
+        let talle = document.querySelectorAll('.check-talle')
         let form = document.querySelector('form')
         
-
+        
+        // console.log(value);
 
         nombre.focus();
 
@@ -13,21 +18,64 @@ window.onload = function() {
 
             // NOMBRE
             if (nombre.value == "") {
-                error.push("El campo nombre es obligatorio")
+                error.push("El campo producto es obligatorio")
             } else if (nombre.value.length < 5) {
                 error.push("El campo nombre debe tener al menos 5 caracteres")
             } else {
                 description.focus();
             }
            
+            // MARCA
+            let marcaValue = marca.options[marca.selectedIndex].innerHTML
+            let texto = "- Seleccione la marca -"
+
+            if (marcaValue == texto) {
+                error.push("Debes asignar una marca")
+            }
+ 
+            // CATEGORIA
+            let checkeado = false
+            
+            for (let i = 0; i < categoria.length; i++) {
+                if (categoria[i].checked) {
+                    checkeado = true
+                }    
+            }
+            if (checkeado == false ) {
+                error.push("Debes asignar una categoria")
+            } else {
+                // imagen.focus();
+            }
+
             // DESCRIPCION
             if (description.value == "") {
                 error.push("El campo descripción es obligatorio")
             } else if (description.value.length < 20) {
                 error.push("El campo descripción debe tener al menos 20 caracteres")
             } else {
-                imagen.focus();
+                price.focus();
             }
+
+             
+            // TALLE          
+            for (let i = 0; i < talle.length; i++) {
+                if (talle[i].checked) {
+                    checkeado = true
+                }    
+            }
+            if (checkeado == false ) {
+                error.push("Debes asignar un talle")
+            } else {
+                // imagen.focus();
+            }
+
+            // PRICE
+            if (price.value == "") {
+                error.push("Debes asignar un precio")
+            } else {
+                // imagen.focus();
+            }
+
 
             // IMAGEN
             if (imagen.files.length == 0) {
@@ -58,4 +106,17 @@ window.onload = function() {
             }
            
         })
+
+        // COUNTER
+        let campoDescripcion = document.getElementById("description");
+        let numero = document.getElementById("numero");
+        numero.innerHTML = campoDescripcion.value.length;
+      
+        campoDescripcion.addEventListener("input", () => {
+          const recordar = campoDescripcion.value.length;
+          const color = recordar < 20 ? "#f2294e" : null;
+      
+          numero.innerHTML = `${recordar}`;
+          numero.style.color = color;
+        });
 }
